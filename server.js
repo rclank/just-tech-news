f52a78b264b8fe7624e049c06e24d65b723e9333
rclank <53981655+rclank@users.noreply.github.com>
2022-01-15 22:12:29 -0500

    partials and helpers

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/helpers');
 
 // import handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 // import session
 const session = require('express-session');
